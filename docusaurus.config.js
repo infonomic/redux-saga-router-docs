8da13256fb4589ebe3874b7bcf272c4177931062
Anthony Bouch <anthony@infonomic.io>
2022-01-17 03:30:49 +0700

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'infonomic', // Usually your GitHub org/user name.
   projectName: 'redux-saga-router ', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
