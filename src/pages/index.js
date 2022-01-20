c5e7b0f76f36b5185210a422ca12edf16b69dcb2
Anthony Bouch <anthony@infonomic.io>
2022-01-20 15:00:57 +0700

    feat: added router hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,18 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import RouterImage from '../assets/images/router.svg'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">Redux-Saga Router</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/introduction">
-            Documentation
+        <div className={styles.hero__image}>
+          <Link to="/docs/introduction">
+            <RouterImage/>
           </Link>
         </div>
       </div>
